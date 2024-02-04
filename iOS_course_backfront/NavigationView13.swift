f6c5892196eaf1d36697a81fe01005431d864adb
Mello Birkan <mgbirkan@icloud.com>
2024-02-03 22:29:50 -0300

    Aula 12 adicionada.

---
+++ iOS_course_backfront/NavigationView13.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationView13.swift
+//  iOS_course_backfront
+//
+//  Created by Marcello Gonzatto Birkan on 03/02/24.
+//
+
+import SwiftUI
+
+struct NavigationView13: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationView13()
+}
